131ce3c32a5e90059cdf046ae7ff36f5f089af37
Pablo A. Roufogalis <pabloroufogalis@proton.me>
2026-01-29 03:08:55 -0500

    Change server

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-e89393e6-3f51-4c80-bcb4-8e694dab4af0.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-d9e342f4-3d82-4c81-93d7-7845f303a0cd.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
